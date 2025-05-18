2fd9c7be5fa91340704ee1df590e57c6aac00fc6
Anushka Sharma <s10.anushka@gmail.com>
2025-05-18 21:14:18 +0530

    isAuthenticated middleware'

--- server/src/routes/ride.route.ts
+++ server/src/routes/ride.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { getShortestPathWithFare } from "../controllers/ride.controller";
+import isAuthenticated from "../middlewares/isAuthenticated";
 
 const router = express.Router();
 
-router.route("/shortest-path").get(getShortestPathWithFare);
+router.route("/shortest-path").get(isAuthenticated, getShortestPathWithFare);
 
 export default router;
